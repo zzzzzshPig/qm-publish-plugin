64ba9ed08d1b36f856c5ed7ae0a9699c5532ef34
zhoushaohui <1012352205@qq.com>
2021-05-08 15:59:57 +0800

    Feat: 注释修改

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import fs from 'fs'
 
-// shelljs/shell.js存在require动态包，插件无法导入只能替换url，所以在这里覆盖掉shell.js的实现
+// shelljs/shell.js存在require动态包，插件无法导入文件只能替换url，所以在这里覆盖掉shell.js的实现
 fs.writeFileSync('./node_modules/shelljs/shell.js', fs.readFileSync('./src/shell.js'))
 
 function baseConfig () {
