b72b55295867ed5814e39e99ed614745e5d4c919
zhoushaohui <1012352205@qq.com>
2021-05-08 18:54:44 +0800

    Feat: 正解有问题，尝试修改

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import copydir from 'copy-dir'
+
+// 需要依赖src目录，copy之
+copydir.sync('./node_modules/shelljs/src', './dist/src')
 
 function baseConfig () {
     return {
@@ -13,7 +17,7 @@ function baseConfig () {
         },
         plugins: [
             commonjs({
-                ignore: ['conditional-runtime-dependency']
+                ignoreDynamicRequires: true
             }),
             terser(),
             typescript()
